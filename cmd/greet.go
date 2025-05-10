52191c6288c7a49a1ebc351b854b16f340cd36a6
Jolonto <aluyapeter7@gmail.com>
2025-05-10 15:40:55 +0100

    respect first commit

---
+++ cmd/greet.go
@@ -0,0 +1,49 @@
+package cmd
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+var name string
+var shout bool
+
+// greetCmd represents the greet command
+var greetCmd = &cobra.Command{
+	Use:   "greet",
+	Short: "Respect CLI for multiple purposes",
+	Long: `Respect CLI was created for learning purpose and also as a guide for my 'Using Cobra in Go' beginner's article I'm working on`,
+
+	Run: func(cmd *cobra.Command, args []string) {
+		if name == "" {
+			name = "stranger"
+		}
+
+		greeting := fmt.Sprintf("Hello, %s", name)
+
+		if shout {
+			greeting = strings.ToUpper(greeting)
+		}
+
+		fmt.Println(greeting)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(greetCmd)
+
+	greetCmd.Flags().StringVarP(&name, "name", "n", "", "Name to greet")
+	greetCmd.Flags().BoolVarP(&shout, "shout", "s", false, "Shout the greeting in uppercase")
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// greetCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// greetCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
