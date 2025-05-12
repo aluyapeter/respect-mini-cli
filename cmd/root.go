3d2e2d679dce464ddd795651ffb477b1d37adbca
Jolonto <aluyapeter7@gmail.com>
2025-05-12 17:02:40 +0100

    small twicth

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
@@ -10,18 +9,11 @@ import (
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "respect-cli",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Respect CLI for multiple purposes",
+	Long: `Respect CLI was created for learning purpose and also as a guide for my 'Using Cobra in Go' beginner's article I'm working on`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
